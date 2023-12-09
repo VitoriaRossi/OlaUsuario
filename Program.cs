b1ace541fa86932d77c5081e5f6d3149f8a27c5f
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-12-09 13:51:11 -0300

    Realização da atividade.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nomeUsuario;
+Console.Write("Digite seu nome: ");
+nomeUsuario = Console.ReadLine();
+Console.WriteLine($"Olá, {nomeUsuario}!");
